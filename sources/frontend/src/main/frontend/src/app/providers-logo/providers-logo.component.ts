c080913fb31d80a81c792b7be8ae2f71ed346a90
HSAINA MOHAMED <mohamedhsaina@gmail.com>
2020-05-27 16:53:42 +0200

    logos bug

--- sources/frontend/src/main/frontend/src/app/providers-logo/providers-logo.component.ts
+++ sources/frontend/src/main/frontend/src/app/providers-logo/providers-logo.component.ts
@@ -21,6 +21,7 @@ export class ProvidersLogoComponent implements OnInit {
   constructor(public providerService: ProviderService, private dialog: MatDialog, public dialogRef: MatDialogRef<ProvidersLogoComponent>,@Inject(MAT_DIALOG_DATA) public data : string) { }
 
   ngOnInit(): void {
+    this.nomSociete=this.nomSociete.split(' ').join(',');
     this.urlsTab = this.providerService.getLogoo(this.nomSociete);
     this.loading=true;
     this.urlsTab.forEach(element=>{
@@ -36,6 +37,7 @@ export class ProvidersLogoComponent implements OnInit {
     this.dialogRef.close(this.data);
   }
   onKeyup(value: string){
+    value=value.split(' ').join(',');
     this.urlsTab = this.providerService.getLogoo(value);
     this.loading=true;
     this.urlsTab.forEach(element=>{
